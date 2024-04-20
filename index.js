5da063485e25ffc589f85ea79b13cb778d2519d1
Lenora Porter <lporter630@gmail.com>
2024-04-19 20:35:02 -0400

    Create a thread

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { openai } from "./config.js";
 
 // Assistant variables
 const asstID = "asst_dBIqrEYjifXIFVsOtNhB2cYN";
+const threadID = "thread_GfIAhRtGKCoNx1EppGAQmshT";
 
 // Upload a file with an "assistants" purpose
 // const file = await openai.files.create({
@@ -11,16 +12,20 @@ const asstID = "asst_dBIqrEYjifXIFVsOtNhB2cYN";
 // console.log(file)
 
 // Create Movie Expert Assistant
-async function createAssistant() {
-	const myAssistant = await openai.beta.assistants.create({
-		instructions:
-			"You are great at recommending movies. When asked a question, use the information in the provided file to form a friendly response. If you cannot find the answer in the file, do your best to infer what the answer should be.",
-		name: "Movie Expert",
-		tools: [{ type: "retrieval" }],
-		model: "gpt-4-1106-preview",
-		file_ids: ["file-vsLQ9OUZJ159LPMschrH0LB9"],
-	});
+// async function createAssistant() {
+// 	const myAssistant = await openai.beta.assistants.create({
+// 		instructions:
+// 			"You are great at recommending movies. When asked a question, use the information in the provided file to form a friendly response. If you cannot find the answer in the file, do your best to infer what the answer should be.",
+// 		name: "Movie Expert",
+// 		tools: [{ type: "retrieval" }],
+// 		model: "gpt-4-1106-preview",
+// 		file_ids: ["file-vsLQ9OUZJ159LPMschrH0LB9"],
+// 	});
 
-	console.log(myAssistant);
-}
-createAssistant();
+// 	console.log(myAssistant);
+// }
+// createAssistant();
+
+// Create a thread
+// const thread = await openai.beta.threads.create();
+// console.log(thread);
